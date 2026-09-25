41b31c91ac080a5fec3e559f9680b25d5ab91723
vulementor <vulementor@gmail.com>
2026-09-25 18:04:42 +0700

    chore(comicreels): pin GPT FullProxy upload-hydration click fix

--- scripts/run_comicreels.ps1
+++ scripts/run_comicreels.ps1
@@ -13,7 +13,7 @@ $RuntimeRef = if ($env:COMICREELS_RUNTIME_REF) { $env:COMICREELS_RUNTIME_REF } e
 $GptfpRequiredRef = if ($env:COMICREELS_GPTFP_REF) {
   $env:COMICREELS_GPTFP_REF
 } else {
-  "89227adf4e6cc3e9d48d965090eca784a318ba2e"
+  "c1144cdbbfc7bc4481e0cd9dca65c30d9112912f"
 }
 $GptfpBranch = if ($env:COMICREELS_GPTFP_BRANCH) { $env:COMICREELS_GPTFP_BRANCH } else { "feature/comicreels-image-attachments" }
 $GptfpRepo = if ($env:COMICREELS_GPTFP_REPO) { $env:COMICREELS_GPTFP_REPO } else { "https://github.com/vulementor/gpt_fullproxy.git" }
