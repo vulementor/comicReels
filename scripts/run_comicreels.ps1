65dce20cfe1d85c068a94a70a608e5c4b5c96ee5
vulementor <vulementor@gmail.com>
2026-09-25 17:47:36 +0700

    chore(comicreels): align Windows GPT FullProxy pin

--- scripts/run_comicreels.ps1
+++ scripts/run_comicreels.ps1
@@ -13,7 +13,7 @@ $RuntimeRef = if ($env:COMICREELS_RUNTIME_REF) { $env:COMICREELS_RUNTIME_REF } e
 $GptfpRequiredRef = if ($env:COMICREELS_GPTFP_REF) {
   $env:COMICREELS_GPTFP_REF
 } else {
-  "a9fdb42903cca43d3d089a61eb05e66230a3ff44"
+  "f1a470acd80ef8196d64d50a010749cc6003acfc"
 }
 $GptfpBranch = if ($env:COMICREELS_GPTFP_BRANCH) { $env:COMICREELS_GPTFP_BRANCH } else { "feature/comicreels-image-attachments" }
 $GptfpRepo = if ($env:COMICREELS_GPTFP_REPO) { $env:COMICREELS_GPTFP_REPO } else { "https://github.com/vulementor/gpt_fullproxy.git" }
