9941ae08d7e3be3dbfee3f9fcb58a1228af6a440
vulementor <vulementor@gmail.com>
2026-09-25 17:58:28 +0700

    chore(comicreels): align Windows assistant completion pin

--- scripts/run_comicreels.ps1
+++ scripts/run_comicreels.ps1
@@ -13,7 +13,7 @@ $RuntimeRef = if ($env:COMICREELS_RUNTIME_REF) { $env:COMICREELS_RUNTIME_REF } e
 $GptfpRequiredRef = if ($env:COMICREELS_GPTFP_REF) {
   $env:COMICREELS_GPTFP_REF
 } else {
-  "f1a470acd80ef8196d64d50a010749cc6003acfc"
+  "89227adf4e6cc3e9d48d965090eca784a318ba2e"
 }
 $GptfpBranch = if ($env:COMICREELS_GPTFP_BRANCH) { $env:COMICREELS_GPTFP_BRANCH } else { "feature/comicreels-image-attachments" }
 $GptfpRepo = if ($env:COMICREELS_GPTFP_REPO) { $env:COMICREELS_GPTFP_REPO } else { "https://github.com/vulementor/gpt_fullproxy.git" }
