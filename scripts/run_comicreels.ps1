4f988f6f758847ba357cd97b300c869c3ecc89a3
vulementor <vulementor@gmail.com>
2026-09-26 21:55:33 +0700

    fix(comicreels): pin modern image-mode verification

--- scripts/run_comicreels.ps1
+++ scripts/run_comicreels.ps1
@@ -13,7 +13,7 @@ $RuntimeRef = if ($env:COMICREELS_RUNTIME_REF) { $env:COMICREELS_RUNTIME_REF } e
 $GptfpRequiredRef = if ($env:COMICREELS_GPTFP_REF) {
   $env:COMICREELS_GPTFP_REF
 } else {
-  "90b990065a74a226a977e92a8f753ac4ee4247b8"
+  "b4374ed2cc62982b4dc1e1862d92c59a65cc9894"
 }
 $GptfpBranch = if ($env:COMICREELS_GPTFP_BRANCH) { $env:COMICREELS_GPTFP_BRANCH } else { "feature/comicreels-image-attachments" }
 $GptfpRepo = if ($env:COMICREELS_GPTFP_REPO) { $env:COMICREELS_GPTFP_REPO } else { "https://github.com/vulementor/gpt_fullproxy.git" }
