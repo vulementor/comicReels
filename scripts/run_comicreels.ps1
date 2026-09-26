3da4d90166daa39810738084c5ba06eb0fb486e9
vulementor <vulementor@gmail.com>
2026-09-26 08:53:48 +0700

    chore(comicreels): pin named attachment resolver

--- scripts/run_comicreels.ps1
+++ scripts/run_comicreels.ps1
@@ -13,7 +13,7 @@ $RuntimeRef = if ($env:COMICREELS_RUNTIME_REF) { $env:COMICREELS_RUNTIME_REF } e
 $GptfpRequiredRef = if ($env:COMICREELS_GPTFP_REF) {
   $env:COMICREELS_GPTFP_REF
 } else {
-  "2c1554bc042661cb0b1a2d254fcf95bc1487f433"
+  "c0c91e69250452c968ac35eeb232dc641b009af5"
 }
 $GptfpBranch = if ($env:COMICREELS_GPTFP_BRANCH) { $env:COMICREELS_GPTFP_BRANCH } else { "feature/comicreels-image-attachments" }
 $GptfpRepo = if ($env:COMICREELS_GPTFP_REPO) { $env:COMICREELS_GPTFP_REPO } else { "https://github.com/vulementor/gpt_fullproxy.git" }
